3cbea2237e3789190b45b9e8fda9293b62172953
Jakub Mikians <2897102+jakub-m@users.noreply.github.com>
2024-09-03 22:01:48 +0200

    other command

--- src/command_push.rs
+++ src/command_push.rs
@@ -1,3 +1,5 @@
+use std::process;
+
 use crate::storage::Storage;
 use regex::Regex;
 pub struct PushCommand<'a> {
@@ -21,4 +23,42 @@ fn sanitize_branch_name(branch_name: &str) -> String {
         .map(|s| s.as_str())
         .unwrap_or(branch_name)
         .to_string()
+}
+
+#[derive(Debug)]
+pub struct PushCommandArgs {
+    pub branch_name: Option<String>,
+}
+
+impl PushCommandArgs {
+    pub fn from_args(args: &Vec<String>) -> Result<PushCommandArgs, String> {
+        let mut output = PushCommandArgs { branch_name: None };
+        let mut iter_args = args.iter();
+        iter_args.next();
+
+        while let Some(arg) = iter_args.next() {
+            if arg == "-h" {
+                PushCommandArgs::print_help();
+                process::exit(0);
+            } else if output.branch_name.is_none() {
+                output = PushCommandArgs {
+                    branch_name: Some(arg.to_string()),
+                    ..output
+                }
+            } else {
+                return Err(format!("unknown param {:?}", arg));
+            }
+        }
+        if output.branch_name.is_none() {
+            return Err("branch missing".to_string());
+        }
+        Ok(output)
+    }
+
+    fn print_help() {
+        let help = "
+Pass branch name to push.
+";
+        println!("{}", help.trim())
+    }
 }
