e61de4fd8032dfe9a560774114af4713e8415679
Jakub Mikians <2897102+jakub-m@users.noreply.github.com>
2024-09-04 22:33:02 +0200

    wip

--- src/app.rs
+++ src/app.rs
@@ -6,7 +6,7 @@ use crate::storage::JsonStorage;
 
 pub fn run() -> Result<(), String> {
     let args = Args::from_args(&env::args().collect())?;
-    let storage = JsonStorage {};
+    let storage = JsonStorage::new("tmp.json")?;
     let command_str = args.command.expect("command missing");
     let command = match command_str.as_ref() {
         "push" => PushCommand::from_args(&args.args, &storage),
--- src/storage.rs
+++ src/storage.rs
@@ -1,11 +1,41 @@
+use serde_json::{json, Value};
+use std::{
+    fs::File,
+    io::{Read, Write},
+};
+
 pub trait Storage {
     fn store_branch_prefix(&self, branch_prefix: &str) -> Result<(), String>;
 }
-pub struct JsonStorage {}
+pub struct JsonStorage {
+    filepath: String,
+}
+
+impl JsonStorage {
+    pub fn new(filepath: &str) -> Result<JsonStorage, String> {
+        if let Ok(mut file) = File::create_new(&filepath) {
+            file.write_all("{}".as_bytes()).map_err(|err| {
+                format!("failed to instantiate storage file {}: {}", filepath, err)
+            })?;
+        }
+        Ok(JsonStorage {
+            filepath: filepath.to_owned(),
+        })
+    }
+}
 
 impl Storage for JsonStorage {
     fn store_branch_prefix(&self, branch_prefix: &str) -> Result<(), String> {
         dbg!(branch_prefix); // TODO
+
+        let mut file = File::open(&self.filepath)
+            .map_err(|err| format!("cannot open storage file {}: {}", self.filepath, err))?;
+        let mut content = String::new();
+        file.read_to_string(&mut content)
+            .map_err(|err| format!("cannot read storage file {}: {}", self.filepath, err))?;
+        let mut json_data: Value = serde_json::from_str(&content)
+            .map_err(|err| format!("storage file is not valid JSON {}: {}", self.filepath, err))?;
+        // todo here
         Ok(())
     }
 }
