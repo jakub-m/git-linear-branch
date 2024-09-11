c6ea2abbc2b327d0819bbdb05261f87bea10847a
Jakub Mikians <2897102+jakub-m@users.noreply.github.com>
2024-09-11 08:33:37 +0200

    minor

--- src/app.rs
+++ src/app.rs
@@ -6,9 +6,11 @@ use crate::command_list::ListCommand;
 use crate::command_push::PushCommand;
 use crate::storage::JsonStorage;
 
+const DEFAULT_STORAGE_FILENAME: &str = ".meta-for-git.json";
+
 pub fn run() -> Result<(), String> {
     let args = Args::from_args(&env::args().collect())?;
-    let storage = JsonStorage::new("tmp.json")?;
+    let storage = JsonStorage::new(DEFAULT_STORAGE_FILENAME)?;
     let command_str = args.command.expect("command missing");
     let command: Box<dyn Command> = match command_str.as_ref() {
         "push" => Box::new(PushCommand::from_args(&args.args, &storage)?),
